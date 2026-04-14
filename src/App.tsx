2d0435c406a44ef25f45d0466965e4307f2c8a4c
Yifan Goh <gohyf91@gmail.com>
2026-04-14 09:00:38 +0800

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import AboutUs from "./pages/AboutUs";
 import Careers from "./pages/Careers";
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Analytics />
+      <SpeedInsights />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
